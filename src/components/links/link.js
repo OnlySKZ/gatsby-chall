c92bc847c61b7337974c2c7c35c1f66c77df9d8a
Florian <meyer.florian79@gmail.com>
2023-12-22 12:21:08 +0100

    Commit

---
+++ src/components/links/link.js
@@ -0,0 +1,8 @@
+const links = [
+  { text: "Home", to: "/" },
+  { text: "Prestations", to: "prestation" },
+  { text: "Representation", to: "#representation" },
+  { text: "Mentions Légales", to: "#mention" },
+];
+
+export default links;
---
+++ src/components/prestations/prestations.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function Prestations() {
+  return (
+    <section id="representation" className="representation">
+      <div className="container">
+        <div className="section-title">
+          <h2>Representation</h2>
+          <p>Bienvenue sur la page Representation</p>
+          <p>Venez voir mes pestacles de magie, c tro kool !!!!! !! !! </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Prestations;
